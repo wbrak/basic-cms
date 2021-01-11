739aa4ae2ba6fab6a63d7a93831a48b89f8bdcef
ivan <webmasterbrak@gmail.com>
2021-01-11 14:16:56 +0100

    refactor routes web

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,14 @@ use App\Http\Controllers\ConnectController;
 use App\Http\Controllers\ContentController;
 use Illuminate\Support\Facades\Route;
 
-
 /*Route::get('/', function () {
     return view('welcome');
 });*/
 
-// Content Blog
+// Content Web
 Route::get('/', [ContentController::class, 'getHome'])->name('Home');
 Route::get('/about', [ContentController::class, 'getAbout'])->name('About');
 Route::get('/contact', [ContentController::class, 'getContact'])->name('Contact');
-/*Route::get('/', 'ContentController@getHome')->name('Home');
-Route::get('/about', 'ContentController@getAbout')->name('About');
-Route::get('/contact', 'ContentController@getContact')->name('Contact');*/
 
 // Created symlink for production in shared server
 Route::get("/storage-link",function(){$targetFolder=storage_path("app/public");$linkFolder=$_SERVER['DOCUMENT_ROOT'].'/storage';symlink($targetFolder,$linkFolder);});
@@ -31,13 +27,3 @@ Route::get('/register', [ConnectController::class, 'getFormRegister'])->name('re
 Route::post('/register', [ConnectController::class, 'postFormRegister'])->name('register');
 Route::get('/register/verify/{code}', [ConnectController::class, 'getVerify'])->name('verify');
 Route::get('/logout', [ConnectController::class, 'getLogout'])->name('logout');
-//Route::get('/login', 'ConnectController@getFormLogin')->name('login');
-/*Route::post('/login', 'ConnectController@postLogin')->name('login');
-Route::get('/recover', 'ConnectController@getRecover')->name('recover');
-Route::post('/recover', 'ConnectController@postRecover')->name('recover');
-Route::get('/reset', 'ConnectController@getReset')->name('reset');
-Route::post('/reset', 'ConnectController@postReset')->name('reset');
-Route::get('/register', 'ConnectController@getRegister')->name('register');
-Route::post('/register', 'ConnectController@postRegister')->name('register');
-Route::get('/register/verify/{code}', 'ConnectController@verify')->name('verify');
-Route::get('/logout', 'ConnectController@getLogout')->name('logout');*/
